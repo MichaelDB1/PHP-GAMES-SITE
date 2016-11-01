363618be9d458ec256cc87ed631b5a2f99c1297e
MichaelDB1 <michaelbarr@hotmail.co.nz>
2016-11-01 20:03:30 +1300

    message update fix

--- UpdateMessages.php
+++ UpdateMessages.php
@@ -2,21 +2,41 @@
 
 
     require 'database.php';
+	require 'CheckEntry.php';
 
     //correct this later
 
-    //security needed to be added
-    $username = $_GET['username'];
+    //security needed to be added				
+    $myusername = $_GET['username'];
 
-    $messages = $_GET['message'];
+    $mymessage = $_GET['message'];
 
-    if ($username == "" || $messages == ""){
-        die();
-    }
 
-    $result = $conn->prepare("INSERT INTO messages VALUES('',?,?)");
-    $result->bind_param("ss", $username, $messages);
-    $result->execute();
+     //$myusername = 'something';
+     //$mymessage = 'something';
+	 
+	 $username = new Checker( $myusername );
+     $username = $username->result();
+	 
+	 $message = new Checker( $mymessage );
+     $message = $message->result();
+     
+	 
 
+    $username = $conn->real_escape_string($username);
+    $message = $conn->real_escape_string($message);
+
+    
+	///fixed this mike... took awhile before i realised that message wasn't meant to be plural
+	$sql = "INSERT INTO messages (username, message) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_Param("ss", $username, $message);
+	
+	
+
+    $stmt->execute();
+	
+	
+	$stmt->close();
 
 ?>
